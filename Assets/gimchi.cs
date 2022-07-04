ba5455e4d6edfbd3fe8982d7771306176bf9edff
MayoneJY <mayone6063@kakao.com>
2022-07-04 20:55:36 +0900

    #남정연 gimchi 스크립트 주석 추가

--- Assets/gimchi.cs
+++ Assets/gimchi.cs
@@ -7,7 +7,7 @@ public class gimchi : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //test
     }
 
     // Update is called once per frame
