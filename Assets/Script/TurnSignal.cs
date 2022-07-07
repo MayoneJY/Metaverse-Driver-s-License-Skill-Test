9a877aad10f5bcc91f1bf2d1350fdbf419cd5c6a
MayoneJY <mayone6063@kakao.com>
2022-07-08 02:52:28 +0900

    #남정연 방향지시등 반복 스크립트

--- Assets/Script/TurnSignal.cs
+++ Assets/Script/TurnSignal.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
 
 public class TurnSignal : MonoBehaviour
 {
+    public Material m_Material_On;
+    public Material m_Material_Off;
+    private Renderer m_Renderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Renderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        m_Renderer.material = m_Material_On;
+        Debug.Log("On");
+        Thread.Sleep(1000);
+        m_Renderer.material = m_Material_Off;
+        Debug.Log("Off");
+        Thread.Sleep(1000);
     }
 }
