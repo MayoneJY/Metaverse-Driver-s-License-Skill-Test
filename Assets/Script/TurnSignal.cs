f76648607615b64bdf4d0acd26454092209908d8
MayoneJY <32291392+MayoneJY@users.noreply.github.com>
2022-07-08 04:19:26 +0900

    #남정연 키보드 입력시 방향지시등, 비상등 끄고 킴
    Q : 왼쪽 방향지시등
    E : 오른쪽 방향지시등
    R : 비상등

--- Assets/Script/TurnSignal.cs
+++ Assets/Script/TurnSignal.cs
@@ -7,39 +7,94 @@ public class TurnSignal : MonoBehaviour
 {
     public Material m_Material_On;
     public Material m_Material_Off;
-    private Renderer m_Renderer;
+    public GameObject m_GameObject_Left;
+    public GameObject m_GameObject_Right;
+    private Renderer m_Renderer_Left;
+    private Renderer m_Renderer_Right;
     private float timer;
     private int waitingTime;
-    private bool lightBool = false;
+    private bool leftTurnSignal = false;
+    private bool leftLightBool = false;
+    private bool rightTurnSignal = false;
+    private bool rightLightBool = false;
+    private bool doubleTurnSignal = false;
     // Start is called before the first frame update
     void Start()
     {
         timer = 0.0f;
         waitingTime = 1;
-        m_Renderer = GetComponent<Renderer>();
+        m_Renderer_Left = m_GameObject_Left.GetComponent<Renderer>();
+        m_Renderer_Right = m_GameObject_Right.GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
 
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            turnSignalOnOff("LEFT");
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            turnSignalOnOff("RIGHT");
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            turnSignalOnOff("DOUBLE");
+        }
+        timer += Time.deltaTime;
         if (timer > waitingTime)
         {
-            //Action
             timer = 0;
-            if (lightBool)
+            if (leftTurnSignal)
             {
-                m_Renderer.material = m_Material_Off;
-                Debug.Log("Off");
-                lightBool = !lightBool;
+                turnSignal(m_Renderer_Left, ref leftLightBool);
             }
-            else
+            if (rightTurnSignal)
             {
-                m_Renderer.material = m_Material_On;
-                Debug.Log("On");
-                lightBool = !lightBool;
+                turnSignal(m_Renderer_Right, ref rightLightBool);
+            }
+            if (doubleTurnSignal)
+            {
+                turnSignal(m_Renderer_Right, ref rightLightBool);
+                turnSignal(m_Renderer_Left, ref leftLightBool);
             }
         }
+        
+
+    }
+
+    private void turnSignal(Renderer trunSignal, ref bool lightBool)
+    {
+
+        if (lightBool)
+        {
+            trunSignal.material = m_Material_Off;
+            lightBool = !lightBool;
+            Debug.Log("Off");
+        }
+        else
+        {
+            trunSignal.material = m_Material_On;
+            lightBool = !lightBool;
+            Debug.Log("On");
+        }
+    }
+
+    private void turnSignalOnOff(string signal)
+    {
+        //한쪽 방향지시등이 켜져있을 때 다른 방향지시등이 꺼짐
+        //비상등 켜질 때 우선 양쪽 방향지시등을 끔
+        if(signal == "LEFT") leftTurnSignal = !leftTurnSignal;
+        else leftTurnSignal = false;
+        leftLightBool = false;
+        if (signal == "RIGHT") rightTurnSignal = !rightTurnSignal;
+        else rightTurnSignal = false;
+        rightLightBool = false;
+        if (signal == "DOUBLE") doubleTurnSignal = !doubleTurnSignal;
+        else doubleTurnSignal = false;
+        m_Renderer_Left.material = m_Material_Off;
+        m_Renderer_Right.material = m_Material_Off;
     }
 }
