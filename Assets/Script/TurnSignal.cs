ce0e74e18338f385682eba7633b1fd1239f73203
MayoneJY <32291392+MayoneJY@users.noreply.github.com>
2022-07-08 03:34:24 +0900

    #남정연 방향지시등 반복 방식 변경

--- Assets/Script/TurnSignal.cs
+++ Assets/Script/TurnSignal.cs
@@ -8,20 +8,38 @@ public class TurnSignal : MonoBehaviour
     public Material m_Material_On;
     public Material m_Material_Off;
     private Renderer m_Renderer;
+    private float timer;
+    private int waitingTime;
+    private bool lightBool = false;
     // Start is called before the first frame update
     void Start()
     {
+        timer = 0.0f;
+        waitingTime = 1;
         m_Renderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        m_Renderer.material = m_Material_On;
-        Debug.Log("On");
-        Thread.Sleep(1000);
-        m_Renderer.material = m_Material_Off;
-        Debug.Log("Off");
-        Thread.Sleep(1000);
+        timer += Time.deltaTime;
+
+        if (timer > waitingTime)
+        {
+            //Action
+            timer = 0;
+            if (lightBool)
+            {
+                m_Renderer.material = m_Material_Off;
+                Debug.Log("Off");
+                lightBool = !lightBool;
+            }
+            else
+            {
+                m_Renderer.material = m_Material_On;
+                Debug.Log("On");
+                lightBool = !lightBool;
+            }
+        }
     }
 }
