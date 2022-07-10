410d70552d88d8e1f71a8b80af236c1390a946cc
Choijiha <ifyouone@naver.com>
2022-07-10 23:22:13 +0900

    #최지하 야간등 상향등 작업
    NightLamp 스크립트 작성
     - J, K 키로 야간등,상향등 킬 수 있음
    
    CivilianVehicle05 프리팹 수정
     - 야간등 상향등 추가
     - Nightlamp 스크립트 추가
    
    SampleScene 수정
     - Terrain 추가

--- Assets/Script/NightLamp.cs
+++ Assets/Script/NightLamp.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class NightLamp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject _nightBeamLight = null;
+    [SerializeField] private GameObject _highBeamLight = null;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            bool isActive = _nightBeamLight.activeSelf == false;
+            _nightBeamLight.SetActive(isActive);
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            bool isActive = _highBeamLight.activeSelf == false;
+            _highBeamLight.SetActive(isActive);
+        }
     }
 }
