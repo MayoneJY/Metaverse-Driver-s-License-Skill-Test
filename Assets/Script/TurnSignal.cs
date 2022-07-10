a46e953433a4356bfe89f86f49b70ad9eb3d4a32
MayoneJY <32291392+MayoneJY@users.noreply.github.com>
2022-07-11 02:42:03 +0900

    #남정연 Light On/Off 스크립트 추가

--- Assets/Script/TurnSignal.cs
+++ Assets/Script/TurnSignal.cs
@@ -13,6 +13,8 @@ public class TurnSignal : MonoBehaviour
     public GameObject m_GameObject_Light_Left_Rear;
     public GameObject m_GameObject_Light_Right_Front;
     public GameObject m_GameObject_Light_Right_Rear;
+    private GameObject[] m_GameObject_Lights_Left;
+    private GameObject[] m_GameObject_Lights_Right;
     private Renderer m_Renderer_Left;
     private Renderer m_Renderer_Right;
     private float timer;
@@ -29,6 +31,8 @@ public class TurnSignal : MonoBehaviour
         waitingTime = 1;
         m_Renderer_Left = m_GameObject_Left.GetComponent<Renderer>();
         m_Renderer_Right = m_GameObject_Right.GetComponent<Renderer>();
+        m_GameObject_Lights_Left = new GameObject[] { m_GameObject_Light_Left_Front, m_GameObject_Light_Left_Rear};
+        m_GameObject_Lights_Right = new GameObject[] { m_GameObject_Light_Right_Front, m_GameObject_Light_Right_Rear};
     }
 
     // Update is called once per frame
@@ -53,35 +57,39 @@ public class TurnSignal : MonoBehaviour
             timer = 0;
             if (leftTurnSignal)
             {
-                turnSignal(m_Renderer_Left, ref leftLightBool);
+                turnSignal(m_Renderer_Left, ref leftLightBool, m_GameObject_Lights_Left);
             }
             if (rightTurnSignal)
             {
-                turnSignal(m_Renderer_Right, ref rightLightBool);
+                turnSignal(m_Renderer_Right, ref rightLightBool, m_GameObject_Lights_Right);
             }
             if (doubleTurnSignal)
             {
-                turnSignal(m_Renderer_Right, ref rightLightBool);
-                turnSignal(m_Renderer_Left, ref leftLightBool);
+                turnSignal(m_Renderer_Right, ref rightLightBool, m_GameObject_Lights_Right);
+                turnSignal(m_Renderer_Left, ref leftLightBool, m_GameObject_Lights_Left);
             }
         }
         
 
     }
 
-    private void turnSignal(Renderer trunSignal, ref bool lightBool)
+    private void turnSignal(Renderer trunSignal, ref bool lightBool, GameObject[] _GameObject_Lights)
     {
 
         if (lightBool)
         {
             trunSignal.material = m_Material_Off;
             lightBool = !lightBool;
+            _GameObject_Lights[0].SetActive(false);
+            _GameObject_Lights[1].SetActive(false);
             Debug.Log("Off");
         }
         else
         {
             trunSignal.material = m_Material_On;
             lightBool = !lightBool;
+            _GameObject_Lights[0].SetActive(true);
+            _GameObject_Lights[1].SetActive(true);
             Debug.Log("On");
         }
     }
@@ -98,6 +106,10 @@ public class TurnSignal : MonoBehaviour
         rightLightBool = false;
         if (signal == "DOUBLE") doubleTurnSignal = !doubleTurnSignal;
         else doubleTurnSignal = false;
+        m_GameObject_Light_Left_Front.SetActive(false);
+        m_GameObject_Light_Left_Rear.SetActive(false);
+        m_GameObject_Light_Right_Front.SetActive(false);
+        m_GameObject_Light_Right_Rear.SetActive(false);
         m_Renderer_Left.material = m_Material_Off;
         m_Renderer_Right.material = m_Material_Off;
     }
