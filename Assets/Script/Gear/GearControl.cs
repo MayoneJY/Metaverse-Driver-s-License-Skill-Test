1e144c66efda1a75fb2a7fcc4134c9735fc8b912
elin_oh <ggomajae@gmail.com>
2022-07-11 22:01:06 +0900

    #오병일 recent upload
    merge test

--- Assets/Script/Gear/GearControl.cs
+++ Assets/Script/Gear/GearControl.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class GearControl : MonoBehaviour
 {
+    Animator animator;
+    int gearValue, gearState;
+
+    void Awake()
+    {
+        gearValue = GetComponent<Gear>();
+        animator = GetComponent<animator>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,9 @@ public class GearControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            
+        }
     }
 }
