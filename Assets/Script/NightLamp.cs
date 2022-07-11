f40a07b081c97d806188ff54f268b1c4cf50c01f
Choijiha <ifyouone@naver.com>
2022-07-11 21:10:42 +0900

    #최지하 야간등 상향등 메테리얼 추가
    CivilianVehicle05 프리팹 수정
    -메테리얼 추가
    -NightLamp 스크립트 수정

--- Assets/Script/NightLamp.cs
+++ Assets/Script/NightLamp.cs
@@ -6,21 +6,50 @@ public class NightLamp : MonoBehaviour
 {
     [SerializeField] private GameObject _nightBeamLight = null;
     [SerializeField] private GameObject _highBeamLight = null;
-
+    [SerializeField] private MeshRenderer _renderer = null;
+    [SerializeField] private Material _lightOn = null;
+    [SerializeField] private Material _lightOff = null;
+    [SerializeField] private GameObject _DayTimeLight = null;
+    [SerializeField] private GameObject _MainLight_LOD0 = null;
 
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKeyDown(KeyCode.J))
         {
             bool isActive = _nightBeamLight.activeSelf == false;
             _nightBeamLight.SetActive(isActive);
+            
+            if (isActive)
+            {
+                _MainLight_LOD0.GetComponent<Renderer>().material = _lightOn;
+            }
+            else
+            {
+                _MainLight_LOD0.GetComponent<Renderer>().material = _lightOff;
+            }
+
+
         }
+
+       
 
         if (Input.GetKeyDown(KeyCode.K))
         {
             bool isActive = _highBeamLight.activeSelf == false;
             _highBeamLight.SetActive(isActive);
+
+            if (isActive)
+            {
+                _MainLight_LOD0.GetComponent<Renderer>().material = _lightOn;
+            }
+            else
+            {
+                _MainLight_LOD0.GetComponent<Renderer>().material = _lightOff;
+            }
         }
+    
+
     }
 }
