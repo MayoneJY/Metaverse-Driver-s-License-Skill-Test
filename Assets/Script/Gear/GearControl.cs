699780a8131929b268c5d4bca3823a5caac9de27
BlackNoir5 <103233073+BlackNoir5@users.noreply.github.com>
2022-07-22 15:45:30 +0900

    #오병일 화면세팅 및 스크립트 변경
    스크립트 변경 화면 세팅 변경
    +GPS카메라
    +사용자시점
    +기어 스크립트변경

--- Assets/Script/Gear/GearControl.cs
+++ Assets/Script/Gear/GearControl.cs
@@ -1,29 +1,30 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GearControl : MonoBehaviour
 {
-    Animator animator;
-    int gearValue, gearState;
+    private string[] GearState = { "Paking", "Return", "Nature", "Drive" };
+    private int GearIndex;
 
-    void Awake()
-    {
-        gearValue = GetComponent<Gear>();
-        animator = GetComponent<animator>();
-    }
     // Start is called before the first frame update
     void Start()
     {
-        
+            
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            
-        }
+        
     }
+
+    public void setGearIndex(int GearIndex){
+        this.GearIndex = GearIndex;
+    }
+
+    public string getGearState(){
+        return GearState[GearIndex];
+    }
+
+    
 }
