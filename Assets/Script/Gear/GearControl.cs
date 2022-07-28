8dc077d4f70fd0c1734489197ffa483b7649a30f
박준수 <90320418+Dohalang@users.noreply.github.com>
2022-07-29 02:30:43 +0900

    #박준수

--- Assets/Script/Gear/GearControl.cs
+++ Assets/Script/Gear/GearControl.cs
@@ -3,28 +3,102 @@ using UnityEngine;
 
 public class GearControl : MonoBehaviour
 {
-    private string[] GearState = { "Paking", "Return", "Nature", "Drive" };
-    private int GearIndex;
+
+    public GameObject Gear;
+
+    private bool ParkingLocation = false;
+    private bool ReturnLocation = false;
+    private bool NatureLocation = false;
+    private bool DriveLocation = false;
 
     // Start is called before the first frame update
     void Start()
     {
-            
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            GearLocationControl("Parking");
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GearLocationControl("Return");
+        }
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            GearLocationControl("Nature");
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            GearLocationControl("Drive");
+        }
     }
 
-    public void setGearIndex(int GearIndex){
-        this.GearIndex = GearIndex;
+    private void CarMove()
+    {
+        if(ParkingLocation == true)
+        {
+           
+        }
+
+        if (ReturnLocation == true)
+        {
+
+        }
+
+        if (NatureLocation == true)
+        {
+
+        }
+
+        if (DriveLocation == true)
+        {
+
+        }
     }
 
-    public string getGearState(){
-        return GearState[GearIndex];
+    private void GearLocationControl(string gear) 
+    { 
+        if(gear == "Parking")
+        {
+            ParkingLocation = true;
+            ReturnLocation = false;
+            NatureLocation = false;
+            DriveLocation = false;
+        }
+
+        if (gear == "Return")
+        {
+            ReturnLocation = true;
+            ParkingLocation = false;
+            NatureLocation = false;
+            DriveLocation = false;
+        }
+
+        if (gear == "Nature")
+        {
+            NatureLocation = true;
+            ReturnLocation = false;
+            ParkingLocation = false;
+            DriveLocation = false;
+        }
+
+        if (gear == "Drive")
+        {
+            DriveLocation = true;
+            ReturnLocation = false;
+            NatureLocation = false;
+            ParkingLocation = false;
+        }
     }
 
-    
+
 }
