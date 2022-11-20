367f47f1046d0ca9519fffcb3407ec01b9d0d7d0
Choijiha <ifyouone@naver.com>
2022-11-20 21:51:52 +0900

    #최지하 기어 조정
    -기어 각도 조절

--- Assets/Script/VR/HandController.cs
+++ Assets/Script/VR/HandController.cs
@@ -5,26 +5,85 @@ using System;
 
 public class HandController : MonoBehaviour
 {
+    public enum GEAR_MODE
+    {
+        PARK,
+        REVERSE,
+        NEUTRAL,
+        DRIVE,
+        MAX
+    }
+
     public Transform topOfLever;
 
     [SerializeField] private float forwardBackwardTilt = 0;
     [SerializeField] private float sideToSideTilt = 0;
-    // Update is called once per frame
+
     void Update()
     {
         forwardBackwardTilt = topOfLever.rotation.eulerAngles.x;
         if(forwardBackwardTilt < 355 && forwardBackwardTilt > 290){
             forwardBackwardTilt = Math.Abs(forwardBackwardTilt - 360);
-            Debug.Log("Backward" + forwardBackwardTilt);
         }
-        else if (forwardBackwardTilt > 5 && forwardBackwardTilt < 74){
-            Debug.Log("Forward" + forwardBackwardTilt);
+    }
+
+    private GEAR_MODE GetGearMode(float angle)
+    {
+        GEAR_MODE result = GEAR_MODE.DRIVE;
+
+        var intAngle = Mathf.RoundToInt(angle);
+
+        if (intAngle < 270)
+        {
+            result = GEAR_MODE.PARK;
         }
+        else if (intAngle < 285 && intAngle >= 270)
+        {
+            result = GEAR_MODE.REVERSE;
+        }
+        else if (intAngle < 300 && intAngle >= 285)
+        {
+            result = GEAR_MODE.NEUTRAL;
+        }
+        else 
+        {
+            result = GEAR_MODE.DRIVE;
+        }
+
+        return result;
+    }
+
+    private float GetGearStickAngle(GEAR_MODE gearMode)
+    {
+        switch (gearMode)
+        {
+            case GEAR_MODE.PARK:
+                return 255;
+            case GEAR_MODE.REVERSE:
+                return 270;
+            case GEAR_MODE.NEUTRAL:
+                return 285;
+            default:
+                return 300;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        var vector = transform.position - topOfLever.position;
+        var angle = Math.Abs(Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg - 180);
+
+        var gearMode = GetGearMode(angle);
+        topOfLever.rotation.Set(GetGearStickAngle(gearMode), 0, 0, 0);
+
     }
 
     private void OnTriggerStay(Collider other) {
         if(other.CompareTag("PlayerHand")){
-            transform.LookAt(other.transform.position, transform.up);
+            var vector = transform.position - other.transform.position;
+            var angle = Math.Abs(Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg );
+            transform.rotation = Quaternion.Euler(angle, -90, 0);
+            //transform.LookAt(other.transform.position, transform.up);
         }
     }
 }
