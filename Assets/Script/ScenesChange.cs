255846e277f23e1991047166bd455d16006c1162
박준수 <90320418+Dohalang@users.noreply.github.com>
2022-11-21 18:49:44 +0900

    #박준수

--- Assets/Script/ScenesChange.cs
+++ Assets/Script/ScenesChange.cs
@@ -7,6 +7,14 @@ public class ScenesChange : MonoBehaviour
     [SerializeField] private GameObject Practice;
     [SerializeField] private GameObject Main;
 
+    public void Start()
+    {
+        SceneManager.LoadScene("");
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
     public void PracticeBtn()
     {
         Main.SetActive(false);
