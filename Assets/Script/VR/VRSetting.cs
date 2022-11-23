b27230b1053c18608674b486970a4a11f6b407dd
MayoneJY <mayone6063@kakao.com>
2022-11-23 11:27:13 +0900

    #남정연
    수정

--- Assets/Script/VR/VRSetting.cs
+++ Assets/Script/VR/VRSetting.cs
@@ -6,10 +6,12 @@ public class VRSetting : MonoBehaviour
 {
     [SerializeField] private GameObject _oculusObject;
     [SerializeField] private GameObject _carObject;
+    public GearControl GC;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GC = _carObject.GetComponent<GearControl>();
+
     }
 
     // Update is called once per frame
@@ -22,16 +24,27 @@ public class VRSetting : MonoBehaviour
         if (other.gameObject.name == "Up")
         {
             var po = _oculusObject.transform.position;
-            _oculusObject.transform.position = new Vector3(po.x, po.y + 0.05f, po.z);
+            _oculusObject.transform.position = new Vector3(po.x, po.y + 0.01f, po.z);
         }
         else if (other.gameObject.name == "Down")
         {
             var po = _oculusObject.transform.position;
-            _oculusObject.transform.position = new Vector3(po.x, po.y - 0.05f, po.z);
+            _oculusObject.transform.position = new Vector3(po.x, po.y - 0.01f, po.z);
         }
         else if (other.gameObject.name == "Respawn")
         {
             _carObject.transform.position = new Vector3(0, 0, 0);
+            _carObject.transform.localEulerAngles = new Vector3(0, 0, 0);
+        }
+        else if(other.gameObject.name == "Drive")
+        {
+            GC.m_GearState_Now = 3;
+
+        }
+        else if(other.gameObject.name == "Return")
+        {
+            GC.m_GearState_Now = 1;
+
         }
 
     }
