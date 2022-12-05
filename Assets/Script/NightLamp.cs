6cf24ec12b94dc2edae2dc8bf129cb5db503b3e4
MayoneJY <mayone6063@kakao.com>
2022-12-05 16:40:36 +0900

    #남정연

--- Assets/Script/NightLamp.cs
+++ Assets/Script/NightLamp.cs
@@ -11,9 +11,12 @@ public class NightLamp : MonoBehaviour
     [SerializeField] private Material _lightOff = null;
     [SerializeField] private GameObject _DayTimeLight = null;
     [SerializeField] private GameObject _MainLight_LOD0 = null;
+    [SerializeField] private GameObject _Ui_Down = null;
+    [SerializeField] private GameObject _Ui_Up = null;
     public bool _nightBeamStatus = false;
     public bool _highBeamStatus = false;
 
+    
     // Update is called once per frame
     void Update()
     {
@@ -41,13 +44,17 @@ public class NightLamp : MonoBehaviour
         {
             _MainLight_LOD0.GetComponent<Renderer>().material = _lightOn;
             _nightBeamLight.SetActive(!_highBeamStatus);
+            _Ui_Down.SetActive(!_highBeamStatus);
             _highBeamLight.SetActive(_highBeamStatus);
+            _Ui_Up.SetActive(_highBeamStatus);
         }
         else
         {
             _MainLight_LOD0.GetComponent<Renderer>().material = _lightOff;
             _nightBeamLight.SetActive(false);
             _highBeamLight.SetActive(false);
+            _Ui_Down.SetActive(false);
+            _Ui_Up.SetActive(false);
         }
     }
 
@@ -56,6 +63,8 @@ public class NightLamp : MonoBehaviour
             _highBeamStatus = _highBeamLight.activeSelf == false;
             _highBeamLight.SetActive(_highBeamStatus);
             _nightBeamLight.SetActive(!_highBeamStatus);
+            _Ui_Up.SetActive(_highBeamStatus);
+            _Ui_Down.SetActive(!_highBeamStatus);
         }
     }
 }
--- Assets/Script/TurnSignal.cs
+++ Assets/Script/TurnSignal.cs
@@ -17,6 +17,11 @@ public class TurnSignal : MonoBehaviour
     private GameObject[] m_GameObject_Lights_Right;
     private Renderer m_Renderer_Left;
     private Renderer m_Renderer_Right;
+    [SerializeField] private GameObject m_GameObject_Left_Ui;
+    [SerializeField] private GameObject m_GameObject_Right_Ui;
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _audio1;
+    [SerializeField] private AudioClip _audio2;
     private float timer;
     private int waitingTime;
     public bool leftTurnSignal = false;
@@ -28,7 +33,7 @@ public class TurnSignal : MonoBehaviour
     void Start()
     {
         timer = 0.0f;
-        waitingTime = 1;
+        waitingTime = 0.5f;
         m_Renderer_Left = m_GameObject_Left.GetComponent<Renderer>();
         m_Renderer_Right = m_GameObject_Right.GetComponent<Renderer>();
         m_GameObject_Lights_Left = new GameObject[] { m_GameObject_Light_Left_Front, m_GameObject_Light_Left_Rear};
@@ -57,23 +62,23 @@ public class TurnSignal : MonoBehaviour
             timer = 0;
             if (leftTurnSignal)
             {
-                turnSignal(m_Renderer_Left, ref leftLightBool, m_GameObject_Lights_Left);
+                turnSignal(m_Renderer_Left, ref leftLightBool, m_GameObject_Lights_Left, m_GameObject_Left_Ui);
             }
             if (rightTurnSignal)
             {
-                turnSignal(m_Renderer_Right, ref rightLightBool, m_GameObject_Lights_Right);
+                turnSignal(m_Renderer_Right, ref rightLightBool, m_GameObject_Lights_Right, m_GameObject_Right_Ui);
             }
             if (doubleTurnSignal)
             {
-                turnSignal(m_Renderer_Right, ref rightLightBool, m_GameObject_Lights_Right);
-                turnSignal(m_Renderer_Left, ref leftLightBool, m_GameObject_Lights_Left);
+                turnSignal(m_Renderer_Right, ref rightLightBool, m_GameObject_Lights_Right, m_GameObject_Right_Ui);
+                turnSignal(m_Renderer_Left, ref leftLightBool, m_GameObject_Lights_Left, m_GameObject_Left_Ui);
             }
         }
         
 
     }
 
-    private void turnSignal(Renderer trunSignal, ref bool lightBool, GameObject[] _GameObject_Lights)
+    private void turnSignal(Renderer trunSignal, ref bool lightBool, GameObject[] _GameObject_Lights, GameObject _GameObject_Lights_Arrow)
     {
 
         if (lightBool)
@@ -82,7 +87,10 @@ public class TurnSignal : MonoBehaviour
             lightBool = !lightBool;
             _GameObject_Lights[0].SetActive(false);
             _GameObject_Lights[1].SetActive(false);
+            _GameObject_Lights_Arrow.SetActive(false);
             Debug.Log("Off");
+            _audioSource.clip = _audio2;
+            _audioSource.Play();
         }
         else
         {
@@ -90,6 +98,9 @@ public class TurnSignal : MonoBehaviour
             lightBool = !lightBool;
             _GameObject_Lights[0].SetActive(true);
             _GameObject_Lights[1].SetActive(true);
+            _GameObject_Lights_Arrow.SetActive(true);
+            _audioSource.clip = _audio1;
+            _audioSource.Play();
             Debug.Log("On");
         }
     }
@@ -110,6 +121,8 @@ public class TurnSignal : MonoBehaviour
         m_GameObject_Light_Left_Rear.SetActive(false);
         m_GameObject_Light_Right_Front.SetActive(false);
         m_GameObject_Light_Right_Rear.SetActive(false);
+        m_GameObject_Left_Ui.SetActive(false);
+        m_GameObject_Right_Ui.SetActive(false);
         m_Renderer_Left.material = m_Material_Off;
         m_Renderer_Right.material = m_Material_Off;
     }
